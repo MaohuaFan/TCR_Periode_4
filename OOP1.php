e4d062be692a973b0111088bdebbd3d7ce291189
MaohuaFan <9019910@student.zadkine.nl>
2023-05-09 11:00:38 +0200

    Create OOP1.php

---
+++ OOP1.php
@@ -0,0 +1,14 @@
+<?php
+    class Product
+    {
+
+    }
+
+    $frisdrank1 = new Product();
+    $frisdrank2 = new Product();
+    $frisdrank3 = new Product();
+
+    var_dump($friskdark1);
+    var_dump($friskdark2);
+    var_dump($friskdark3);
+?>

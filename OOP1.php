5908031d25aaca58d4d76dce71895e84e96b6e19
MaohuaFan <9019910@student.zadkine.nl>
2023-05-09 11:02:01 +0200

    php

--- OOP1.php
+++ OOP1.php
@@ -8,7 +8,7 @@
     $frisdrank2 = new Product();
     $frisdrank3 = new Product();
 
-    var_dump($friskdark1);
-    var_dump($friskdark2);
-    var_dump($friskdark3);
+    var_dump($frisdrank1);
+    var_dump($frisdrank2);
+    var_dump($frisdrank3);
 ?>

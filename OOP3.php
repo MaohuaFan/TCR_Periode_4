d3012206432573fe854197e216d00bad7e434fbc
MaohuaFan <9019910@student.zadkine.nl>
2023-05-09 11:12:52 +0200

    php

---
+++ OOP3.php
@@ -0,0 +1,32 @@
+<?php
+    class Product
+    {
+        public $name = "Een bepaald frisdrank";
+    }
+
+    $frisdrank1 = new Product();
+    $frisdrank1->name = "Frisdrank 1";
+
+    $frisdrank2 = new Product();
+    $frisdrank2->name = "Frisdrank 2";
+
+    $frisdrank3 = new Product();
+    $frisdrank3->name = "Frisdrank 3";
+    
+    $frisdrank4 = new Product();
+    $frisdrank4->name = "Frisdrank 4";
+
+
+    echo $frisdrank1->name. "<br>";
+    echo $frisdrank2->name. "<br>";
+    echo $frisdrank3->name. "<br>";
+    echo $frisdrank4->name. "<br>";
+    
+    $frisdrank1->name = "Frisdrank_1";
+    echo $frisdrank1->name. "<br>";
+
+    var_dump($frisdrank1);
+    var_dump($frisdrank2);
+    var_dump($frisdrank3);
+    var_dump($frisdrank4);
+?>

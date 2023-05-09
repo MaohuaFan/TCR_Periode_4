74200ff0f1dc8a8717f9b0fcf17b2cf84a522efc
MaohuaFan <9019910@student.zadkine.nl>
2023-05-09 12:12:29 +0200

    php

---
+++ Opdrachten_OOP/PHP_OOP_opdracht_7-9/OOP7.php
@@ -0,0 +1,40 @@
+<?php
+    class Product
+    {
+        public $name;
+        public $price;
+        public $currency;
+
+        public function __construct($price, $name = "een muzieksoort", $currency = "&euro")
+        {
+            $this->name = ucfirst($name);
+            $this->price = $price;
+            $this->currency = $currency;
+        }
+
+        public function formatPrice()
+        {
+            return number_format($this->price, decimals:2);
+        }
+
+        public function getProduct()
+        {
+            return "Het product ".$this->name." kost ".$this->currency." ".$this->price;
+        }
+    }
+
+    $muzieksoort1 = new Product(price:40, currency:"€", name:"muzieksoort 1");
+    $muzieksoort1->getProduct();
+    //$muzieksoort2 = new Product(price:10, name:"muzieksoort 2");
+
+//    echo "<br>".$muzieksoort1->name."<br>";
+//    echo $muzieksoort1->currency."<br>";
+//    echo $muzieksoort1->price."<br><br>";
+
+    //echo $muzieksoort2->formatPrice(). "<br>";
+    //echo $muzieksoort2->name. "<br>";
+    //echo $muzieksoort2->price. "<br><br>";
+    
+    var_dump($muzieksoort1);
+    //var_dump($muzieksoort2);
+?>
